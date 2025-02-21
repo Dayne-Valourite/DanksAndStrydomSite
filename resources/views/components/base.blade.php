770d42efad782379139360dba82fa2c50ab805db
Dayne-Valourite <dayne@valourite.co.za>
2025-02-21 13:48:28 +0200

    added the node modules

---
+++ resources/views/components/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('storage/images/icons/Hydraulics_Icon.png') }}">
+
+    {{-- common scripts --}}
+    @vite('resources/css/app.css')
+</head>
+<body class="h-full">
+    {{ $slot }}
+</body>
+</html>
