61847f6394e84f8f490b49755541c2f48fd2c01b
Dayne-Valourite <dayne@valourite.co.za>
2025-02-21 18:55:30 +0200

    updated header and other components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
 <x-header>
-    <h1 class="bg-black w-full text-white">Hello World!</h1>
+    <main   class="min-h-screen pt-[50px]" 
+            style=" background-image: url('{{ asset('storage/images/Logo/danksandstrydom_logo.webp') }}'); 
+                    background-repeat: no-repeat; 
+                    background-size: cover; 
+                    background-position: center;">
+        
+    </main>
 </x-header>
